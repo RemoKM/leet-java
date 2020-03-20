178d70aa65934768e063950e710bd3faf35d6c94
Ashwanth fernando <ashwa@192-168-1-102.tpgi.com.au>
2020-03-20 14:41:59 +1100

    20/03/2020

---
+++ ReverseLinkedList.java
@@ -0,0 +1,26 @@
+
+/*
+leetcode problem : 206
+https://leetcode.com/problems/reverse-linked-list/
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while(head != null){
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
+
+    }
+}
